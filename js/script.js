b13288c9961670b3adfeb944472574b4112b46fb
yanggitak <anthimus3@gmail.com>
2023-03-11 21:54:07 +0900

    헤더, 푸터 완료
    - 가장 심플하고 가독성 있게 처리 노력함.

--- js/script.js
+++ js/script.js
@@ -6,36 +6,5 @@ let nav = $(".ygt-nav"),
 initialize();
 
 function initialize() {
-    setEventTab();
+    
 }
-
-function setEventTab() {
-    $(".tab_content").hide();
-    $(".tab_content:first").show();
-    $(navigation_links[0]).addClass("active");
-
-    highlight.css({
-        left: navigation_links[0].offsetLeft + "px",
-        height: navigation_links[0].offsetHeight + "px",
-        width: navigation_links[0].offsetWidth + "px"
-    });
-
-    $(".ygt-nav a").click(function () {
-        let activeTab = $(this).attr("rel");
-
-        $(".ygt-nav a").removeClass("active");
-        $(this).addClass("active");
-        $(".tab_content").hide()
-        $("#" + activeTab).fadeIn()
-    });
-
-    $(navigation_links).on("click", function () {
-        $(navigation_links).removeClass("active");
-        $(this).addClass("active");
-        highlight.css({
-            left: this.offsetLeft + "px",
-            width: this.offsetWidth + "px"
-        });
-    });
-}
-
